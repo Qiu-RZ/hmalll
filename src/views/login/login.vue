e8853c7083dac8aba2577f4cfef5493188533291
Qiu <qiu@121.com>
2019-11-19 20:56:24 +0800

    登录模块上面

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,37 +1,70 @@
 <template>
-<div class="login-container">
-     <!--左侧  form -->
+  <div class="login-container">
+    <!--左侧  form -->
+    <div class="form-wrapper">
+      <!-- 标题 -->
+      <div class="title-box">
+        <img class="login-logo" src="../../assets/login_icon.png" alt />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
 
-     <div class="form-wrapper"></div>
-
-     <!-- 右侧图片 -->
-     <img src="../../assets/login_banner_ele.png" alt="">
-
-</div>
+    <!-- 右侧图片 -->
+    <img src="../../assets/login_banner_ele.png" alt />
+  </div>
 </template>
 
 <script>
 export default {
-  nama:'login'
-}
+  nama: "login"
+};
 </script>
 
-<style>
-.login-container{
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    height: 100%;
-    background: linear-gradient(
+<style lang = "less">
+.login-container {
+  display: flex;
+  /* 上下居中 */
+  align-items: center;
+  /* 水平方向的对其方式 */
+  justify-content: space-around;
+  height: 100%;
+  background: linear-gradient(
     225deg,
     rgba(1, 198, 250, 1),
     rgba(20, 147, 250, 1)
   );
-}
-.form-wrapper{
-
+  .form-wrapper {
     width: 478px;
     height: 550px;
-    background-color: #F5F5F5;
+    background-color: #f5f5f5;
+    padding-top: 48px;
+    padding-left: 43px;
+    padding-right: 41px;
+    box-sizing: border-box;
+    .title-box {
+      display: flex;
+      align-items: center;
+      .login-logo {
+        width: 22px;
+        height: 17px;
+        margin-right: 14px;
+      }
+      .title {
+        font-size: 24px;
+        margin-right: 14px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-right: 12px;
+      }
+      .sub-title {
+        font-size: 22px;
+      }
+    }
+  }
 }
 </style>
