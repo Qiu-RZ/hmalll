b9812ace293d4187c0fa73345af9aaf54bc1fa6f
Qiu <qiu@121.com>
2019-11-19 20:24:43 +0800

    登录模块整体布局

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+
 
 //导入router
 import router from './router/router.js'
@@ -9,8 +9,14 @@ import router from './router/router.js'
 //导包 element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+//导入自己的全局样式
+import './style/base.css'
 //用包
-Vue.use(ElementUI)
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,13 @@
 <template>
-<div>我是登录页面的占位符</div>
+<div class="login-container">
+     <!--左侧  form -->
+
+     <div class="form-wrapper"></div>
+
+     <!-- 右侧图片 -->
+     <img src="../../assets/login_banner_ele.png" alt="">
+
+</div>
 </template>
 
 <script>
@@ -9,5 +17,21 @@ export default {
 </script>
 
 <style>
+.login-container{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 100%;
+    background: linear-gradient(
+    225deg,
+    rgba(1, 198, 250, 1),
+    rgba(20, 147, 250, 1)
+  );
+}
+.form-wrapper{
 
+    width: 478px;
+    height: 550px;
+    background-color: #F5F5F5;
+}
 </style>
