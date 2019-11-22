b5fb87f0728153ca1fd56080c2a4fa941ed4b2d8
Qiu <qiu@121.com>
2019-11-22 20:56:29 +0800

    首页模块 - 整体布局

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,16 +1,56 @@
 <template>
-  <div>
-      这是首页页面
-  </div>
+  <el-container class="index-container">
+    <!-- 头部 -->
+    <el-header class="header">
+      <div class="left">
+        <i class="el-icon-s-fold"></i>
+        <img class="logo" src="../../assets/index-logo.png" alt />
+        <span class="font">黑马面面</span>
+      </div>
+      <div class="right"></div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    name:'index'
-
-}
+  name: "index"
+};
 </script>
 
-<style>
-
+<style lang="less">
+.index-container {
+  height: 100%;
+    background-color: skyblue;
+    height: 100%;
+    .header {
+      background-color: yellowgreen;
+      display: flex;
+      
+    justify-content: space-between;
+      .left{
+     display:flex;
+    justify-content: space-between;
+    .el-icon-s-fold{
+        width:24px;
+        height:20px;
+    }
+    .logo{
+        width: 33px;
+        height:28px;
+    }
+    .font{
+        font-size: 22px
+    }
+      }
+    }
+    .aside {
+      background-color: pink;
+    }
+ 
+}
 </style>
