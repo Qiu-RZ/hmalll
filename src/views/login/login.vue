55e19c21688266f5895c57e3ee5e4a3c57dcc047
Qiu <qiu@121.com>
2019-11-22 18:20:06 +0800

    表单注册-整体验证完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -25,9 +25,9 @@
           ></el-input>
         </el-form-item>
 
-        <el-form-item label prop="passwrd">
+        <el-form-item label prop="password">
           <el-input
-            v-model="loginForm.passwrd"
+            v-model="loginForm.password"
             placeholder="请输入密码"
             prefix-icon="el-icon-lock"
             class="password-input"
@@ -43,17 +43,17 @@
             </el-col>
           </el-row>
         </el-form-item>
-      </el-form>
 
-      <!-- 协议 -->
-      <el-checkbox class="checkbox" v-model="checked">
-        我已阅读并同意
-        <el-link type="primary">用户协议</el-link>和
-        <el-link type="primary">隐私条款</el-link>
-      </el-checkbox>
-      <!-- 登录 注册按钮 -->
-      <el-button class="login-btn" type="primary" @click="submitForm('loginForm')">登录</el-button>
-      <el-button class="reg-btn" type="primary" @click="dialogFormVisible=true">注册</el-button>
+        <!-- 协议 -->
+        <el-checkbox class="checkbox" v-model="checked">
+          我已阅读并同意
+          <el-link type="primary">用户协议</el-link>和
+          <el-link type="primary">隐私条款</el-link>
+        </el-checkbox>
+        <!-- 登录 注册按钮 -->
+        <el-button class="login-btn" type="primary" @click="submitForm('loginForm')">登录</el-button>
+        <el-button class="reg-btn" type="primary" @click="dialogFormVisible=true">注册</el-button>
+      </el-form>
     </div>
 
     <!-- 右侧图片 -->
@@ -61,69 +61,75 @@
 
     <!-- 模态框 -->
     <el-dialog title="用户注册" :visible.sync="dialogFormVisible" class="imgs">
-      <el-form :model="form">
-        <!-- 上传文件 -->
-        <el-form :model="registerForm">
-          <!-- 头像 -->
-          <el-form-item label="头像" :label-width="formLabelWidth">
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload"
-            >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </el-form-item>
-          <!-- 昵称 -->
-          <el-form-item label="昵称" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
-          </el-form-item>
-          <!-- 邮箱 -->
-          <el-form-item label="邮箱" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
-          </el-form-item>
-          <!-- 手机 -->
-          <el-form-item label="手机" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
-          </el-form-item>
-          <!-- 密码 -->
-          <el-form-item label="密码" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
-          </el-form-item>
+      <!-- 上传文件 -->
+      <el-form :model="registerForm" :rules="loginRureg" ref="registerForm">
+        <!-- 头像 -->
+        <el-form-item label="头像" :label-width="formLabelWidth">
+          <el-upload
+            class="avatar-uploader"
+            action="http://183.237.67.218:3002/uploads"
+            name='image'
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
 
-          <!-- 图形码 -->
-          <el-row>
-            <el-col :span="16">
-              <el-form-item label="图形码" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="7" :offset="1">
-              <img :src="registerURL" alt class="imgsheig" @click = "regURL" />
-            </el-col>
-          </el-row>
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <!-- 昵称 -->
+        <el-form-item label="昵称" :label-width="formLabelWidth" prop="name">
+          <el-input v-model="registerForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 邮箱 -->
+        <el-form-item label="邮箱" :label-width="formLabelWidth" prop="email">
+          <el-input v-model="registerForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 手机 -->
+        <el-form-item label="手机" :label-width="formLabelWidth" prop="phone">
+          <el-input v-model="registerForm.phone" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码" :label-width="formLabelWidth" prop="password">
+          <el-input v-model="registerForm.password" autocomplete="off"></el-input>
+        </el-form-item>
 
-          <!-- 验证码 -->
+        <!-- 图形码 -->
+        <el-row>
+          <el-col :span="16">
+            <el-form-item label="图形码" :label-width="formLabelWidth">
+              <el-input v-model="registerForm.code" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7" :offset="1">
+            <img :src="registerURL" alt class="imgsheig" @click="regURL" />
+          </el-col>
+        </el-row>
 
-          <el-row>
-            <el-col :span="16">
-              <el-form-item label="验证码" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="7" :offset="1">
-              <el-button type="primary" class="btn-width">获取短信信息</el-button>
-            </el-col>
-          </el-row>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-        </div>
+        <!-- 验证码 -->
+
+        <el-row>
+          <el-col :span="16">
+            <el-form-item label="验证码" :label-width="formLabelWidth">
+              <el-input v-model="registerForm.rcode" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7" :offset="1">
+            <!-- 获取手机验证码 -->
+            <el-button
+              type="primary"
+              class="btn-width"
+              @click="getMessage"
+              :disabled="isDisabled"
+            >{{btnTxt}}</el-button>
+          </el-col>
+        </el-row>
       </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="registerUser">确 定</el-button>
+      </div>
     </el-dialog>
   </div>
 </template>
@@ -148,12 +154,30 @@ export default {
         }
       }
     };
+    //检验邮箱格式
+    const checkEmail = (rules, value, callback) => {
+      // value是值
+      if (!value) {
+        callback(new Error("邮箱不能为空"));
+      } else {
+        // 格式验证
+        const reg = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+        // 验证
+        if (reg.test(value)) {
+          // 对的
+          callback();
+        } else {
+          // 错误
+          callback(new Error("邮箱格式不对哦"));
+        }
+      }
+    };
     return {
       loginForm: {
         //手机
         phone: "",
         //密码
-        passwrd: "",
+        password: "",
         //验证码
         captcha: ""
       },
@@ -162,7 +186,7 @@ export default {
         //手机号
         phone: [{ validator: checkAge, required: true }],
         //密码
-        passwrd: [
+        password: [
           { required: true, message: "密码不能为空" },
           { min: 4, max: 8, message: "长度在 4 到 8 个字符" }
         ],
@@ -172,6 +196,7 @@ export default {
           { min: 4, max: 4, message: "4个字符噢" }
         ]
       },
+
       //获取验证码地址
       captchaURL: "http://183.237.67.218:3002/captcha?type=login",
       //复选框按钮
@@ -179,13 +204,38 @@ export default {
       //模态框
       imageUrl: "",
       dialogFormVisible: false,
-      registerForm: "",
-      form: {
-        name: ""
+      // 注册表单数据
+      registerForm: {
+        name: "",
+        phone: "",
+        email: "",
+        // 用户头像
+        avatar: "",
+        password: "",
+        // 短信验证码
+        rcode: "",
+        // 图形验证码
+        code: ""
       },
+      //注册验证规则
+      loginRureg: {
+        //昵称
+        name: [{ required: true }],
+        //手机号码
+        phone: [{ validator: checkAge, required: true }],
+        //邮箱
+        email: [{ validator: checkEmail, required: true }],
+        //密码
+        password: [{ required: true }]
+      },
+
       formLabelWidth: "67px",
       //注册获取验证码
-      registerURL:"http://183.237.67.218:3002/captcha?type=sendsms",
+      registerURL: "http://183.237.67.218:3002/captcha?type=sendsms",
+      //短信验证的文本
+      btnTxt: "获取短信验证码",
+      //获取手机验证按钮
+      isDisabled: false
     };
   },
 
@@ -218,13 +268,13 @@ export default {
             withCredentials: true
           }).then(res => {
             window.console.log(res);
-            // if (res.data.code == 200) {
-            //   // 成功
-            //   this.$message.success("你可算回来啦！");
-            // } else {
-            //   // 失败
-            //   this.$message.warning("登录失败了哦");
-            // }
+            if (res.data.code == 200) {
+              // 成功
+              this.$message.success("你可算回来啦！");
+            } else {
+              // 失败
+              this.$message.warning("登录失败了哦");
+            }
           });
         } else {
           // 验证失败
@@ -235,9 +285,13 @@ export default {
     },
 
     //模态框
+    //文件上传之后走的回调函数
     handleAvatarSuccess(res, file) {
       this.imageUrl = URL.createObjectURL(file.raw);
+        // 保存到表单中
+      this.registerForm.avatar = res.data.file_path;
     },
+    //文件上传之前对文件做一些限制
     beforeAvatarUpload(file) {
       const isJPG = file.type === "image/jpeg";
       const isLt2M = file.size / 1024 / 1024 < 2;
@@ -251,10 +305,77 @@ export default {
       return isJPG && isLt2M;
     },
 
-    //点击冲洗获取注册验证码
-    regURL(){
+    //点击冲重新取注册验证码
+    regURL() {
       this.registerURL = `http://183.237.67.218:3002/captcha?type=sendsms&${Date.now()}`;
     },
+    //获取手机短信之前对手机号做判断
+    getMessage() {
+      if (this.registerForm.phone.trim() == "") {
+        this.$message.warning("手机号不能为空哦");
+        return;
+      }
+      //获取手机验证发送请求
+      axios({
+        url: "http://183.237.67.218:3002/sendsms",
+        method: "post",
+        data: {
+          code: this.registerForm.code,
+          phone: this.registerForm.phone
+        },
+        withCredentials: true
+      }).then(res => {
+        //成功回调
+        window.console.log(res);
+      });
+
+      let time = 60;
+      // 禁用按钮 开启定时器
+      this.isDisabled = true;
+      const interId = setInterval(() => {
+        // 递减
+        time--;
+        // 修改页面
+        this.btnTxt = `${time}S后再次获取`;
+        if (time == 0) {
+          clearInterval(interId);
+          // 重新启用按钮
+          this.isDisabled = false;
+          // 还原文本
+          this.btnTxt = "获取短信验证码";
+        }
+      }, 100);
+
+      //用户注册
+    },
+
+    registerUser() {
+      this.$refs.registerForm.validate(valid => {
+        if (valid) {
+          //  window.console.log(valid);
+          axios({
+            url: "http://183.237.67.218:3002/register",
+            method: "post",
+            data: {
+              avatar: this.registerForm.avatar,
+              email: this.registerForm.email,
+              name: this.registerForm.name,
+              password: this.registerForm.password,
+              phone: this.registerForm.phone,
+              rcode: this.registerForm.rcode
+            }
+          }).then(res => {
+            //成功回调
+            window.console.log(res);
+            alert('成功')
+          });
+        }else{
+           // 验证失败
+          window.console.log("error submit!!");
+          return false;
+        }
+      });
+    }
   }
 };
 </script>
@@ -392,7 +513,6 @@ export default {
     width: 178px;
     height: 178px;
     display: block;
-    /* margin: 0 auto; */
     margin-top: 20px;
   }
 
